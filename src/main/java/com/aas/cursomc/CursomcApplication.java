bb43cc15fe648fa1fb69b725f1d0abc04af543a1
Aluisio Anderson <aaluisio@gmail.com>
2020-01-05 20:33:17 -0300

    Produto e associacao muito para muitos

--- src/main/java/com/aas/cursomc/CursomcApplication.java
+++ src/main/java/com/aas/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.aas.cursomc.domain.Categoria;
+import com.aas.cursomc.domain.Produto;
 import com.aas.cursomc.repositories.CategoriaRepository;
+import com.aas.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired //11-Criar uma dependência de produtoRepository
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +30,24 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		//10-Instâncias da classe Produto
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		//Associações entre as categorias 1 e 2 e os produtos 1, 2 e 3.
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		//Associações entre os produtos 1, 2 e 3 e as categorias 1 e 2.
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		//Salva todos os produtos.
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
--- src/main/java/com/aas/cursomc/domain/Produto.java
+++ src/main/java/com/aas/cursomc/domain/Produto.java
@@ -8,33 +8,45 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-
 //6-Checklist - Serializable (padrão: 1L)
+//7-Entity e Id são mapeamentos
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	//1-Checklist: Atributos básicos
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY) //Para o BD H2
 	private Integer id;
 	private String nome;
-	
-	//2-Checklist - Associação: Uma categoria tem vários produtos (Papel)
+	private Double preco;
+
 	//8-Notação em um dos dois lados. Mapeamento da lista de categorias. Relação muitos para muitos.
-		@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	//2-Checklist - Associação: Uma produto tem uma ou mais categorias (Papel)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	public Categoria() {
+	//3-Checklist - Construtor vazio
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	//3-Checklist - Construtor com parâmetros (não inclua coleções)
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-
+	
+	//4-Checklist - Gerar os getters e setters
 	public Integer getId() {
 		return id;
 	}
@@ -51,15 +63,22 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	//4-Checklist - Gerar os getters e setters
-		public List<Produto> getProdutos() {
-			return produtos;
-		}
+	public Double getPreco() {
+		return preco;
+	}
 
-		public void setProdutos(List<Produto> produtos) {
-			this.produtos = produtos;
-		}
-	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
 	//5-Checklist - hashCode e equals (implementação padrão: somente id): Métodos para comparação
 	@Override
 	public int hashCode() {
@@ -68,7 +87,7 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -77,14 +96,15 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 	
-
+	
+	
 }
